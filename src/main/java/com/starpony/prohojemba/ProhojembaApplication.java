28fd51ad59a4d8703d36b1932c6e2eae6960a7df
umaruch <60070943+umaruch@users.noreply.github.com>
2023-02-26 00:58:40 +0500

    Создан скелет spring boot приложения

---
+++ src/main/java/com/starpony/prohojemba/ProhojembaApplication.java
@@ -0,0 +1,13 @@
+package com.starpony.prohojemba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProhojembaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProhojembaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/starpony/prohojemba/ProhojembaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starpony.prohojemba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProhojembaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
